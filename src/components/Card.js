5f09e6f38f5e514b465065170252d94f4a17c92e
tmaulon <thomas.maulon@gmail.com>
2019-11-30 12:07:53 +0100

    improve cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
 const Card = ({ details }) => {
+
+    const ingredients = details.ingredients
+        .split(',')
+        .map(item => <li key={item}>{item}</li>)
+
+    const instructions = details.instructions
+        .split('\n')
+        .map(item => <li key={item}>{item}</li>)
+
     return (
         <div className="card">
+            <div className="image">
+                <img src={require(`../img/${details.image}`)} alt={details.nom} />
+            </div>
             <div className="recipe">
                 <h2>
                     {details.nom}
                 </h2>
+                <ul className="liste-ingredients">
+                    {ingredients}
+                </ul>
+                <ol>
+                    {instructions}
+                </ol>
             </div>
         </div>
     )
